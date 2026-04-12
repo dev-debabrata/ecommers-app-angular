b5a3936486881b4eeabb3c6e00d157a11ba96f02
Debabrata Das <debabratadas711@gmail.com>
2026-04-12 17:53:38 +0530

    12-04-2026

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [Navbar],
+  imports: [RouterLink, CommonModule, Navbar, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  showMenu = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  logout() {
+    localStorage.removeItem('isLoggedIn');
+    this.isLoggedIn = false;
+    this.showMenu = false;
+    this.router.navigate(['/']);
+  }
+  links = [
+    { name: 'Home', dropdown: false },
+    { name: 'Products', dropdown: true },
+    { name: 'About', dropdown: false },
+    { name: 'Contact', dropdown: false },
+  ];
+
+  openDropdownIndex: number | null = null;
+
+  openDropdown(index: number) {
+    if (this.links[index].dropdown) {
+      this.openDropdownIndex = index;
+    }
+  }
+
+  closeDropdown() {
+    this.openDropdownIndex = null;
+  }
+}
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -5,50 +5,20 @@ import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-  showMenu = false;
-
-  constructor(private router: Router) {}
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
-  }
-
-  logout() {
-    localStorage.removeItem('isLoggedIn');
-    this.isLoggedIn = false;
-    this.showMenu = false;
-    this.router.navigate(['/']);
-  }
-  links = [
-    { name: 'Home', dropdown: false },
-    { name: 'Products', dropdown: true },
-    { name: 'About', dropdown: false },
-    { name: 'Contact', dropdown: false },
+  categories = [
+    'Home',
+    'Fashion',
+    'Electronics',
+    'Bags',
+    'Footwear',
+    'Groceries',
+    'Beauty',
+    'Wellness',
+    'Jewellery',
   ];
-
-  openDropdownIndex: number | null = null;
-
-  openDropdown(index: number) {
-    if (this.links[index].dropdown) {
-      this.openDropdownIndex = index;
-    }
-  }
-
-  closeDropdown() {
-    this.openDropdownIndex = null;
-  }
-  // // categories = ['Home', 'All Products', 'Electronics', 'Fashion', 'Beauty', 'Sports'];
-
-  // links = ['Home', 'Products', 'About', 'Contact'];
-  // isDropdownOpen = false;
-
-  // toggleDropdown() {
-  //   this.isDropdownOpen = !this.isDropdownOpen;
-  // }
 }
