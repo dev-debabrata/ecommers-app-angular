965d538cc1d50cf301294f4723fb1ac26dc2d6d0
Debabrata Das <debabratadas711@gmail.com>
2026-04-13 23:28:42 +0530

    fixed service and header

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -6,20 +6,49 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { Navbar } from '../navbar/navbar';
 import { AuthService } from '../../services/auth-service';
+import { Breadcrumb } from '../breadcrumb/breadcrumb';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../services/product-service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, CommonModule, Navbar, MatIconModule, MatSnackBarModule],
+  imports: [
+    RouterLink,
+    CommonModule,
+    FormsModule,
+    Navbar,
+    MatIconModule,
+    MatSnackBarModule,
+    Breadcrumb,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   private router = inject(Router);
   private authService = inject(AuthService);
+  private productService = inject(ProductService);
   private snackBar = inject(MatSnackBar);
+
+  searchTerm = '';
+  suggestions: any[] = [];
 
   showMenu = false;
   openDropdownIndex: number | null = null;
+
+  // constructor() {
+  //   this.router.events.subscribe((event: any) => {
+  //     if (event?.urlAfterRedirects) {
+  //       const url = event.urlAfterRedirects;
+
+  //       // ❌ clear only when NOT on product pages
+  //       if (!url.startsWith('/products')) {
+  //         this.searchTerm = '';
+  //         this.suggestions = [];
+  //       }
+  //     }
+  //   });
+  // }
 
   get isLoggedIn() {
     return this.authService.isLoggedIn();
@@ -42,4 +71,99 @@ export class Header {
 
     this.router.navigate(['/']);
   }
+
+  onInputChange() {
+    const term = this.searchTerm.trim().toLowerCase();
+
+    if (!term) {
+      this.suggestions = [];
+      return;
+    }
+
+    let products = this.productService.getProductsCache();
+
+    if (products.length === 0) {
+      this.productService.getProducts().subscribe((res: any) => {
+        products = res.products;
+        this.productService.setProductsCache(products);
+        this.filterSuggestions(products, term);
+      });
+    } else {
+      this.filterSuggestions(products, term);
+    }
+  }
+
+  filterSuggestions(products: any[], term: string) {
+    this.suggestions = products.filter((p) => p.title.toLowerCase().includes(term)).slice(0, 5);
+  }
+
+  selectProduct(product: any) {
+    // this.searchTerm = product.title;
+    this.searchTerm = '';
+    this.suggestions = [];
+
+    this.router.navigate(['/products', product.id]);
+  }
+
+  onSearch() {
+    const term = this.searchTerm.trim().toLowerCase();
+    if (!term) return;
+
+    let products = this.productService.getProductsCache();
+
+    if (products.length === 0) {
+      this.productService.getProducts().subscribe((res: any) => {
+        products = res.products;
+        this.productService.setProductsCache(products);
+        this.findAndNavigate(products, term);
+      });
+    } else {
+      this.findAndNavigate(products, term);
+    }
+  }
+
+  findAndNavigate(products: any[], term: string) {
+    const match = products.find((product) => product.title.toLowerCase().includes(term));
+
+    if (match) {
+      this.router.navigate(['/products', match.id]);
+
+      this.searchTerm = '';
+      this.suggestions = [];
+    } else {
+      this.snackBar.open('Product not found', 'Close', {
+        duration: 3000,
+      });
+    }
+  }
+
+  //   get cartCount(): number {
+  //   return this.productService.getCartCount();
+  // }
 }
+
+// findAndNavigate(products: any[], term: string) {
+//   const match = products.find((product) => product.title.toLowerCase().includes(term));
+
+//   if (match) {
+//     this.router.navigate(['/products', match.id]);
+//   } else {
+//     this.snackBar.open('Product not found', 'Close', {
+//       duration: 3000,
+//     });
+//   }
+// }
+
+// onSearch() {
+//   const products = this.productService.getProductsCache();
+
+//   const match = products.find((p: any) =>
+//     p.title.toLowerCase().includes(this.searchTerm.toLowerCase()),
+//   );
+
+//   if (match) {
+//     this.router.navigate(['/products', match.id]);
+//   } else {
+//     alert('Product not found');
+//   }
+// }
--- frontend/src/app/services/product-service.ts
+++ frontend/src/app/services/product-service.ts
@@ -1,8 +1,47 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../models/products';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  
+  private api = 'https://dummyjson.com/products';
+  private http = inject(HttpClient);
+
+  private productsCache: Product[] = [];
+  private cart: any[] = [];
+
+  getProducts(): Observable<Product> {
+    return this.http.get<Product>(`${this.api}?limit=30`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.api}/${id}`);
+  }
+
+  setProductsCache(products: Product[]) {
+    this.productsCache = products;
+  }
+
+  getProductsCache(): Product[] {
+    return this.productsCache;
+  }
+
+  addToCart(product: any) {
+    this.cart.push(product);
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  removeFromCart(index: number) {
+    this.cart.splice(index, 1);
+  }
+
+  clearCart() {
+    this.cart = [];
+  }
 }
