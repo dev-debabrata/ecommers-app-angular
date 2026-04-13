94afc3fb69ff755122310890d61b5c5e50bd13b7
Debabrata Das <debabratadas711@gmail.com>
2026-04-14 00:01:41 +0530

    14-04-2026

--- frontend/src/app/pages/products/product-detail-page/product-detail-page.ts
+++ frontend/src/app/pages/products/product-detail-page/product-detail-page.ts
@@ -1,11 +1,123 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
+import { ProductService } from '../../../services/product-service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Product } from '../../../models/products';
+import { Rating } from '../../../utils/rating.util';
+import { CommonModule } from '@angular/common';
+import { TimeAgoPipe } from '../../../pipes/time-ago-pipe';
+import { Loader } from '../../../components/loader/loader';
+import { Error } from '../../../components/error/error';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TimeAgoPipe, Loader, Error],
   templateUrl: './product-detail-page.html',
   styleUrl: './product-detail-page.css',
 })
 export class ProductDetailPage {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private authService = inject(AuthService);
+  private productService = inject(ProductService);
+  // Automatic cleanup
+  private destroyRef = inject(DestroyRef);
 
+  private snackBar = inject(MatSnackBar);
+
+  // Component State
+  product: Product | null = null;
+  isLoading = true;
+  errorMsg = false;
+  stars: string[] = [];
+  showAllReviews = false;
+  isPopupOpen = false;
+
+  // ProductService to Fatch eatch Products
+  ngOnInit(): void {
+    const productId = Number(this.route.snapshot.paramMap.get('id'));
+
+    const productSub = this.productService.getProductById(productId).subscribe({
+      next: (res) => {
+        this.isLoading = false;
+        this.product = res;
+        console.log(res);
+        this.stars = Rating.getStars(this.product.rating);
+      },
+      error: (err) => {
+        this.errorMsg = true;
+        this.isLoading = false;
+        console.error(err);
+      },
+    });
+
+    // Automatically Component Destroyed
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  addCart(product: any) {
+    const isLoggedIn = this.authService.isLoggedIn();
+
+    // ❌ NOT LOGGED IN
+    if (!isLoggedIn) {
+      this.snackBar.open('Please login to add cart', 'Close', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        panelClass: ['snackbar-error'],
+      });
+
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.productService.addToCart(product);
+
+    this.snackBar.open('Added to cart', 'Close', {
+      duration: 2000,
+    });
+
+    // OPTIONAL: redirect to cart (ONLY if you want)
+    this.router.navigate(['/cart']);
+  }
+
+  // addCart(product: any) {
+  //   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
+  //   if (!isLoggedIn) {
+  //     this.snackBar.open('Please login to add cart', 'Close', {
+  //       duration: 3000,
+  //       horizontalPosition: 'center',
+  //       verticalPosition: 'top',
+  //       panelClass: ['snackbar-error'],
+  //     });
+  //     this.router.navigate(['/login']);
+  //     return;
+  //   }
+
+  //   this.productService.addToCart(product);
+  // }
+
+  // Review
+  toggleReviews(): void {
+    this.showAllReviews = !this.showAllReviews;
+  }
+
+  displayedReviews() {
+    if (!this.product || !this.product.reviews) return [];
+    return this.showAllReviews ? this.product.reviews : [this.product.reviews[0]];
+  }
+
+  // Popup Model view open & close
+  openReviewsPopup(): void {
+    this.isPopupOpen = true;
+  }
+
+  closeReviewsPopup(): void {
+    this.isPopupOpen = false;
+  }
 }
--- frontend/src/app/pages/products/product-list-page/product-list-page.ts
+++ frontend/src/app/pages/products/product-list-page/product-list-page.ts
@@ -1,11 +1,92 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TruncatePipe } from '../../../pipes/truncate-pipe';
+import { Loader } from '../../../components/loader/loader';
+import { Router } from '@angular/router';
+import { ProductService } from '../../../services/product-service';
+import { Product } from '../../../models/products';
+import { Rating } from '../../../utils/rating.util';
+import { Error } from '../../../components/error/error';
+import { Highlight } from '../../../directives/highlight';
 
 @Component({
   selector: 'app-product-list-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, TruncatePipe, Highlight, Loader, Error],
   templateUrl: './product-list-page.html',
   styleUrl: './product-list-page.css',
 })
 export class ProductListPage {
+  // Get Router & ProductService
+  private router = inject(Router);
+  private productService = inject(ProductService);
 
+  // Automatic cleanup
+  private destroyRef = inject(DestroyRef);
+
+  // Component State
+  products: Product[] = [];
+  isLoading = true;
+  errorMsg = false;
+
+  // Search and Filter
+  searchTerm = '';
+  selectedCategory: string = 'All';
+  categories: string[] = [];
+
+  // ProductService to Fatch Products
+  ngOnInit(): void {
+    const productSub = this.productService.getProducts().subscribe({
+      next: (res: any) => {
+        this.isLoading = false;
+        this.products = res.products;
+
+        // Category
+        const categories = Array.from(new Set(this.products.map((p) => p.category)));
+        // this.categories = ['All', ...new Set(this.products.map((p) => p.category))];
+        const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+        this.categories = ['All', ...categories.map(capitalize)];
+
+        console.log(res);
+        // console.log('isLoading:', this.isLoading);
+      },
+
+      error: (err) => {
+        this.errorMsg = true;
+        this.isLoading = false;
+        console.log(err);
+      },
+    });
+    // Automatically Component Destroyed
+    this.destroyRef.onDestroy(() => {
+      productSub.unsubscribe();
+    });
+  }
+
+  // Products search and selected category
+  get filteredProducts() {
+    const search = this.searchTerm.toLowerCase();
+    return this.products.filter((product) => {
+      const matchesSearch = product.title.toLowerCase().includes(search);
+
+      const matchesCategory =
+        this.selectedCategory === 'All' ||
+        product.category.toLowerCase() === this.selectedCategory.toLowerCase();
+      return matchesSearch && matchesCategory;
+      // const matchesCategory =
+      //   this.selectedCategory === 'All' || product.category === this.selectedCategory;
+      // return matchesSearch && matchesCategory;
+    });
+  }
+
+  // Navigate to product details page
+  viewDetails(id: number): void {
+    this.router.navigate(['/products', id]);
+  }
+
+  // Rating
+  get ratingUtil() {
+    return Rating;
+  }
 }
