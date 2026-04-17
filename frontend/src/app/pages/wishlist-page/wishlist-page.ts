ec233382d86e2da2f3bfbcdd240a4739b55b1ea2
Debabrata Das <debabratadas711@gmail.com>
2026-04-17 22:29:33 +0530

    add wishlist

--- frontend/src/app/pages/wishlist-page/wishlist-page.ts
+++ frontend/src/app/pages/wishlist-page/wishlist-page.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WishlistService } from '../../services/wishlist-service';
+import { CartService } from '../../services/cart-service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Rating } from '../../utils/rating.util';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wishlist-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './wishlist-page.html',
   styleUrl: './wishlist-page.css',
 })
 export class WishlistPage {
+  public wishlistService = inject(WishlistService);
+  private cartService = inject(CartService);
 
+  private snackBar = inject(MatSnackBar);
+
+  getRating() {
+    return Rating;
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    this.wishlistService.removeFromWishlist(product.id);
+    this.snackBar.open('Moved to cart', 'Close', {
+      duration: 2000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['snackbar-success'],
+    });
+  }
 }
