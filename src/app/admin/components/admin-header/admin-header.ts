19568ddec21b6f5cd35b0a0a4129203de75cce4c
Debabrata Das <debabratadas711@gmail.com>
2026-04-22 23:57:34 +0530

    22-04-2026

---
+++ src/app/admin/components/admin-header/admin-header.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminAuthService } from '../../../services/admin-auth-service';
+
+@Component({
+  selector: 'app-admin-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-header.html',
+  styleUrl: './admin-header.css',
+})
+export class AdminHeader {
+  private router = inject(Router);
+  private adminAuthService = inject(AdminAuthService);
+
+  logout() {
+    this.adminAuthService.logout();
+  }
+}
