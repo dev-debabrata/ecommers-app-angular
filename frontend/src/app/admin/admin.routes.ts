8c86fbaef14d32a529dd862ef7c54c43e9a3134d
Debabrata Das <debabratadas711@gmail.com>
2026-04-22 23:39:54 +0530

    22-04-2026

--- frontend/src/app/admin/admin.routes.ts
+++ frontend/src/app/admin/admin.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
+
 import { AdminLayout } from '../layouts/admin-layout/admin-layout';
 import { Dashboard } from './dashboard/dashboard';
 import { AdminLogin } from './auth/admin-login/admin-login';
 import { adminAuthGuard } from '../guards/admin-auth-guard';
-// import { ProductList } from './products/product-list/product-list';
-// import { AddProduct } from './products/add-product/add-product';
+import { ProductList } from './products/product-list/product-list';
+import { AddProduct } from './products/add-product/add-product';
+import { Users } from './users/users';
+import { Orders } from './orders/orders';
 
 export const adminRoutes: Routes = [
   {
@@ -21,14 +24,24 @@ export const adminRoutes: Routes = [
         path: '',
         component: Dashboard,
       },
-      // {
-      //   path: 'products',
-      //   component: ProductList,
-      // },
-      // {
-      //   path: 'add-product',
-      //   component: AddProduct,
-      // },
+
+      {
+        path: 'users',
+        component: Users,
+      },
+
+      {
+        path: 'products',
+        children: [
+          { path: '', component: ProductList },
+          { path: 'add-product', component: AddProduct },
+        ],
+      },
+
+      {
+        path: 'orders',
+        component: Orders,
+      },
     ],
   },
 ];
