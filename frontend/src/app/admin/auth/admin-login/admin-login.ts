03533bfda32aa340a2e4bddfde139992882c2f0d
Debabrata Das <debabratadas711@gmail.com>
2026-04-22 23:27:21 +0530

    add admin login

--- frontend/src/app/admin/auth/admin-login/admin-login.ts
+++ frontend/src/app/admin/auth/admin-login/admin-login.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AdminAuthService } from '../../../services/admin-auth-service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './admin-login.html',
   styleUrl: './admin-login.css',
 })
 export class AdminLogin {
+  private router = inject(Router);
+  private adminAuthService = inject(AdminAuthService);
 
+  private snackBar = inject(MatSnackBar);
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const email = this.loginForm.value.email!;
+    const password = this.loginForm.value.password!;
+
+    this.adminAuthService.loginAdmin(email, password).subscribe({
+      next: (res) => {
+        if (res.length > 0) {
+          localStorage.setItem('admin', JSON.stringify(res[0]));
+
+          this.snackBar.open('Admin login Successfully!', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            panelClass: [`snackbar-success`],
+          });
+          this.router.navigate(['/admin']);
+        } else {
+          this.snackBar.open('Invalid credentials', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            panelClass: [`snackbar-error`],
+          });
+        }
+      },
+      error: () => {
+        this.snackBar.open('Server error', 'Close', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top',
+          panelClass: ['snackbar-error'],
+        });
+      },
+    });
+  }
 }
--- frontend/src/app/services/admin-auth-service.ts
+++ frontend/src/app/services/admin-auth-service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Admin } from '../models/admin';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminAuthService {
-  
+  private apiUrl = 'http://localhost:3000/admin';
+  private http = inject(HttpClient);
+  private router = inject(Router);
+
+  loginAdmin(email: string, password: string): Observable<Admin[]> {
+    return this.http.get<Admin[]>(`${this.apiUrl}?email=${email}&password=${password}`);
+  }
+
+  logout() {
+    localStorage.removeItem('admin');
+    this.router.navigate(['/admin/login']);
+  }
 }
