a0bc4d2e4df71436421e7ce5b0b193e0235ae730
Debabrata Das <debabratadas711@gmail.com>
2026-04-25 22:43:58 +0530

    25-04-2026

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,20 +4,56 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+
     provideRouter(
       routes,
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
       }),
     ),
+
     provideHttpClient(),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
+
+// import {
+//   ApplicationConfig,
+//   provideBrowserGlobalErrorListeners,
+//   provideZoneChangeDetection,
+// } from '@angular/core';
+// import { provideRouter, withInMemoryScrolling } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideHttpClient } from '@angular/common/http';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZoneChangeDetection({ eventCoalescing: true }),
+//     provideRouter(
+//       routes,
+//       withInMemoryScrolling({
+//         scrollPositionRestoration: 'top',
+//       }),
+//     ),
+//     provideHttpClient(),
+//   ],
+// };
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBam5UPSCWxkGVgb_j2zAKUs4IEYcEMlvM',
+    authDomain: 'e-commerce-angular-app-b8807.firebaseapp.com',
+    projectId: 'e-commerce-angular-app-b8807',
+    storageBucket: 'e-commerce-angular-app-b8807.firebasestorage.app',
+    messagingSenderId: '371387267091',
+    appId: '1:371387267091:web:44a0e2edf81a537e42e7e6',
+  },
+};
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
