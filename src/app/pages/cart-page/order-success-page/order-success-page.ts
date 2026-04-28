cb9d27256867ecc3aceab5a675cd494dbe9c5847
Debabrata Das <debabratadas711@gmail.com>
2026-04-28 22:47:12 +0530

    28-04-2026

--- src/app/pages/cart-page/order-success-page/order-success-page.ts
+++ src/app/pages/cart-page/order-success-page/order-success-page.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
+import { OrderService } from '../../../services/order-service';
 
 @Component({
   selector: 'app-order-success-page',
@@ -9,22 +11,29 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './order-success-page.html',
   styleUrl: './order-success-page.css',
 })
-export class OrderSuccessPage {
+export class OrderSuccessPage implements OnInit {
+  order: any = null;
+
   private router = inject(Router);
   private route = inject(ActivatedRoute);
-
-  order: any = null;
+  private authService = inject(AuthService);
+  private orderService = inject(OrderService);
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
 
-    const data = localStorage.getItem('orders');
+    this.authService.getFullUser().then((user: any) => {
+      if (!user?.uid || !id) return;
 
-    if (data) {
-      const orders = JSON.parse(data);
+      this.orderService.getOrderById(user.uid, id).subscribe((data) => {
+        if (!data) return;
 
-      this.order = orders.find((o: any) => o.id == id);
-    }
+        this.order = {
+          ...data,
+          date: data.date?.toDate ? data.date.toDate() : data.date,
+        };
+      });
+    });
   }
 
   goHome() {
