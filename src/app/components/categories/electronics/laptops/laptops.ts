df2bfc39a9ffe907271559795e7557b703733bc8
Debabrata Das <debabratadas711@gmail.com>
2026-05-05 22:13:02 +0530

    05-05-2026

---
+++ src/app/components/categories/electronics/laptops/laptops.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laptops',
+  imports: [],
+  templateUrl: './laptops.html',
+  styleUrl: './laptops.css',
+})
+export class Laptops {
+
+}
---
+++ src/app/components/categories/electronics/mobiles/mobiles.ts
@@ -0,0 +1,69 @@
+import { Component, computed, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ProductService } from '../../../../services/product-service';
+import { Product } from '../../../../models/products';
+
+@Component({
+  selector: 'app-mobiles',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './mobiles.html',
+  styleUrl: './mobiles.css',
+})
+export class Mobiles implements OnInit {
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+
+  products = signal<Product[]>([]);
+  currentIndex = signal(0);
+
+  @Input() category: string = 'mobiles';
+  @Input() totalLimit = 24;
+  pageSize = 8;
+
+  fullProducts = computed(() => {
+    return this.products().slice(0, this.totalLimit);
+  });
+
+  limitedProducts = computed(() => {
+    const start = this.currentIndex();
+    const end = start + this.pageSize;
+
+    return this.fullProducts().slice(start, end);
+  });
+
+  ngOnInit() {
+    const sub = this.productService.getProductsByCategory(this.category).subscribe({
+      next: (res: Product[]) => {
+        this.products.set(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => sub.unsubscribe());
+  }
+
+  next() {
+    const maxIndex = this.fullProducts().length - this.pageSize;
+
+    if (this.currentIndex() < maxIndex) {
+      this.currentIndex.set(this.currentIndex() + this.pageSize);
+    }
+  }
+
+  prev() {
+    if (this.currentIndex() > 0) {
+      this.currentIndex.set(this.currentIndex() - this.pageSize);
+    }
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
+}
