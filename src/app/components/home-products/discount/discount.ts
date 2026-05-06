459af87c554c8abb4a05e59d693af8f04e8da4c2
Debabrata Das <debabratadas711@gmail.com>
2026-05-06 07:27:25 +0530

    added

--- src/app/components/home-products/discount/discount.ts
+++ src/app/components/home-products/discount/discount.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, Input, signal } from '@angular/core';
+import { ProductService } from '../../../services/product-service';
+import { Router } from '@angular/router';
+import { Product } from '../../../models/products';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-discount',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
   templateUrl: './discount.html',
   styleUrl: './discount.css',
 })
 export class Discount {
+  private productService = inject(ProductService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
+  @Input() totalLimit = 24;
+  pageSize = 8;
+
+  products = signal<Product[]>([]);
+
+  currentIndex = signal(0);
+
+  fullProducts = computed(() => {
+    return this.products().slice(0, this.totalLimit);
+  });
+
+  limitedProducts = computed(() => {
+    const start = this.currentIndex();
+    const end = start + this.pageSize;
+
+    return this.fullProducts().slice(start, end);
+  });
+
+  ngOnInit() {
+    const sub = this.productService.getDiscountProducts().subscribe({
+      next: (res: Product[]) => {
+        this.products.set(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => sub.unsubscribe());
+  }
+
+  next() {
+    const maxIndex = this.fullProducts().length - this.pageSize;
+
+    if (this.currentIndex() < maxIndex) {
+      this.currentIndex.set(this.currentIndex() + this.pageSize);
+    }
+  }
+
+  prev() {
+    if (this.currentIndex() > 0) {
+      this.currentIndex.set(this.currentIndex() - this.pageSize);
+    }
+  }
+
+  viewDetails(id: string) {
+    this.router.navigate(['/products', id]);
+  }
 }
