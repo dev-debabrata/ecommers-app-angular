fdaec2eb11ef78219871deeca13c9fd3d5fd0dd4
Debabrata Das <debabratadas711@gmail.com>
2026-05-10 20:53:16 +0530

    fixed

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Hero } from '../../components/hero/hero';
-import { TodayDeals } from '../../components/home-products/today-deals/today-deals';
-import { Discount } from '../../components/home-products/discount/discount';
-import { MoreItems } from '../../components/home-products/more-items/more-items';
 import { Mobiles } from '../../components/categories/electronics/mobiles/mobiles';
+import { MoreItems } from '../../components/categories/more-items/more-items';
+import { Discount } from '../../components/categories/discount/discount';
+import { TodayDeals } from '../../components/categories/today-deals/today-deals';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Hero, RouterLink, TodayDeals, Discount, MoreItems, Mobiles],
+  imports: [Hero, RouterLink, Mobiles, MoreItems, Discount, TodayDeals],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
